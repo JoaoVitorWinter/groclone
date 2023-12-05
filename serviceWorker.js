cbbaad27107e95009c12cdd99d41b9ac7956c1e3
JoaoVitorWinter <joaovwinter3108@gmail.com>
2023-12-05 20:19:21 -0300

    Iniciando PWA | 05/12 20:19

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const cacheGroclone = "groclone-v1"
+
+const assets = [
+    "./",
+    "./index.html",
+    "./css/style.css",
+    "./assets/iconFeature1.png"
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(cacheGroclone).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
