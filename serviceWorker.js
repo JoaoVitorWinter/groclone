f16341ffc0562077b2e7437e3fde1d78b59f9769
JoaoVitorWinter <joaovwinter3108@gmail.com>
2023-12-05 21:06:17 -0300

    Update serviceWorker | 05/12 21:06

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const cacheGroclone = "groclone-v1"
+var cacheGroclone = 'groclone-v1';
 
 const assets = [
     "/",
@@ -8,22 +8,65 @@ const assets = [
     "/assets/favicon.ico"
 ]
 
-self.addEventListener("install", installEvent => {
-    console.log("entrou install")
-    installEvent.waitUntil(
-        caches.open(cacheGroclone).then(cache => {
-            cache.addAll(assets)
-        })
-    )
-    console.log("saiu install")
-})
 
-self.addEventListener("fetch", fetchEvent => {
-    console.log("entrou fetch")
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(res => {
-            return res || fetch(fetchEvent.request)
+self.addEventListener('install', function (event) {
+
+  event.waitUntil(
+
+    caches.open(cacheGroclone).then(function (cache) {
+
+      return cache.addAll(assets);
+
+    })
+
+  )
+
+});
+
+
+
+
+self.addEventListener('activate', function activator(event) {
+
+  event.waitUntil(
+
+    caches.keys().then(function (keys) {
+
+      return Promise.all(keys
+
+        .filter(function (key) {
+
+          return key.indexOf(cacheGroclone) !== 0;
+
         })
-    )
-    console.log("saiu fetch")
-})
+
+        .map(function (key) {
+
+          return caches.delete(key);
+
+        })
+
+      );
+
+    })
+
+  );
+
+});
+
+
+
+
+self.addEventListener('fetch', function (event) {
+
+  event.respondWith(
+
+    caches.match(event.request).then(function (cachedResponse) {
+
+      return cachedResponse || fetch(event.request);
+
+    })
+
+  );
+
+});
